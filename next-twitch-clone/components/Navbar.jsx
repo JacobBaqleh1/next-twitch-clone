4465570fb84ef50a637be953d4ff3c91823d7790
Jacob Baqleh <101436252+JacobBaqleh1@users.noreply.github.com>
2023-03-15 21:55:52 -0700

    Installed next and tailwind

---
+++ next-twitch-clone/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Navbar= ()=> {
+  return (
+    <div>
+  <div className="fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-10" >
+    navbar
+  </div>
+  </div>
+  )
+}
+export default Navbar
